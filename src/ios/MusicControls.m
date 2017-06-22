fc0e3f86a24dbf2e60bc4b7203b79e91cef4737e
Jonas Schafft <jonas.schafft@rds.de>
2017-06-22 11:46:43 +0200

    21

--- src/ios/MusicControls.m
+++ src/ios/MusicControls.m
@@ -12,12 +12,8 @@
 @implementation MusicControls
 
 - (void) create: (CDVInvokedUrlCommand *) command {
-    
-    
-    [self deregisterMusicControlsEventListener];
-    [self registerMusicControlsEventListener];
-    
-    NSLog(@"create MusicControls Test 19");
+  
+    NSLog(@"create MusicControls Test 21");
     
     NSDictionary * musicControlsInfoDict = [command.arguments objectAtIndex:0];
     MusicControlsInfo * musicControlsInfo = [[MusicControlsInfo alloc] initWithDictionary:musicControlsInfoDict];
