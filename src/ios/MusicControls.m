822892c1091ed19746e22d702a372fed21b12e31
Jonas Schafft <jonas.schafft@rds.de>
2017-06-22 10:24:53 +0200

    Test 11

--- src/ios/MusicControls.m
+++ src/ios/MusicControls.m
@@ -13,7 +13,7 @@
 
 - (void) create: (CDVInvokedUrlCommand *) command {
     
-    NSLog(@"create MusicControls Test 10");
+    NSLog(@"create MusicControls Test 11");
     
     NSDictionary * musicControlsInfoDict = [command.arguments objectAtIndex:0];
     MusicControlsInfo * musicControlsInfo = [[MusicControlsInfo alloc] initWithDictionary:musicControlsInfoDict];
@@ -57,16 +57,16 @@
     
         commandCenter.nextTrackCommand.enabled = [musicControlsInfo hasNext];
         
-     //   [commandCenter.previousTrackCommand addTarget:self action:@selector(prevSounds)];
-        
-     //   [commandCenter.nextTrackCommand addTarget:self action:@selector(nextSounds)];
+        [commandCenter.previousTrackCommand addTarget:self action:@selector(prevSounds)];
+    
+        [commandCenter.nextTrackCommand addTarget:self action:@selector(nextSounds)];
         
         commandCenter.playCommand.enabled = ![musicControlsInfo isPlaying];
         commandCenter.pauseCommand.enabled = [musicControlsInfo isPlaying];
         
-    //    [commandCenter.playCommand addTarget:self action:@selector(prevSounds)];
+        [commandCenter.playCommand addTarget:self action:@selector(prevSounds)];
         
-    //    [commandCenter.pauseCommand addTarget:self action:@selector(nextSounds)];
+        [commandCenter.pauseCommand addTarget:self action:@selector(nextSounds)];
         
         
         [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
