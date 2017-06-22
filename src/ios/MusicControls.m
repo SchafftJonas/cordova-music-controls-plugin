c50c248af9612395d8e73d9d03616a70ce58bb56
Jonas Schafft <jonas.schafft@rds.de>
2017-06-22 10:49:07 +0200

    Test 14

--- src/ios/MusicControls.m
+++ src/ios/MusicControls.m
@@ -13,7 +13,7 @@
 
 - (void) create: (CDVInvokedUrlCommand *) command {
     
-    NSLog(@"create MusicControls Test 13");
+    NSLog(@"create MusicControls Test 14");
     
     NSDictionary * musicControlsInfoDict = [command.arguments objectAtIndex:0];
     MusicControlsInfo * musicControlsInfo = [[MusicControlsInfo alloc] initWithDictionary:musicControlsInfoDict];
@@ -81,6 +81,10 @@
        
         
         nowPlayingInfoCenter.nowPlayingInfo = updatedNowPlayingInfo;
+        
+        [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+        
+        
          NSLog(@"#### end ####");
        
     }];
